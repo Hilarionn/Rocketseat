cc6841b29ab1beb9d9f0493101ef88b267dcaa1f
Hilarionn <155191160+Hilarionn@users.noreply.github.com>
2025-09-24 19:41:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 function toggleMode() {
-  const html = document.documentElement;
-  html.classList.toggle('light');
+const html = document.documentElement
+html.classList.toggle('light')
 
-  // pegar a tag img
-  const img = document.querySelector("#profile img");
+// pegar a tag img
+const img = document.querySelector("#profile img" )
 
-  // substituir a imagem
-  if (html.classList.contains('light')) {
-    img.setAttribute('src', './Assets/Avatar-light.png');
-  } else {
-    img.setAttribute('src', './Assets/Avatar.png');
-  }
+//substituir a imagem
+if (html.classList.contains('light')) {
+//se tiver light mode, adicionar a imagem light
+img.setAttribute('src', 'Assets/Avatar-Light.png')
+} else {
+    //se tiver sem light mode, manter a imagem normal
+    img.setAttribute('src', 'Assets/Avatar.png')
 }
+
