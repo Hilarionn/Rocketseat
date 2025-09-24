d9aeaface06cfb05c4a7bd549e50d96984ddb059
Hilarionn <155191160+Hilarionn@users.noreply.github.com>
2025-09-24 19:35:48 -0300

    Update script.js - Change-IA

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 function toggleMode() {
-const html = document.documentElement
-html.classList.toggle('light')
+  const html = document.documentElement;
+  html.classList.toggle('light');
 
-// pegar a tag img
-const img = document.querySelector("#profile img" )
+  const img = document.querySelector("#profile img");
 
-//substituir a imagem
-if (html.classList.contains('light')) {
-  img.setAttribute('src', './Assets/Avatar-light.png')
-} else {
-  img.setAttribute('src', './Assets/Avatar.png')
+  if (html.classList.contains('light')) {
+    img.setAttribute('src', './Assets/Avatar-light.png');
+  } else {
+    img.setAttribute('src', './Assets/Avatar.png');
+  }
 }
-
 
 
 /*
