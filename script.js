e781c098c6daa6639d7b3cee4229c26a175ee1ea
Hilarionn <155191160+Hilarionn@users.noreply.github.com>
2025-09-24 23:13:21 +0000

    Troca da URl

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function toggleMode() {
   html.classList.toggle('light')
   const img = document.querySelector("#profile img")
   if (html.classList.contains('light')) {
-    img.setAttribute('src', 'Assets/Avatar-Light.png')
+    img.setAttribute('src', 'Assets/Avatar-light.png')
   } else {
     img.setAttribute('src', 'Assets/Avatar.png')
   }
