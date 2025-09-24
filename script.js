c25a964462d287961381e4215cc4aafd7497fc4b
Hilarionn <155191160+Hilarionn@users.noreply.github.com>
2025-09-24 19:39:58 -0300

    Update script.js - Reverse

--- script.js
+++ script.js
@@ -2,30 +2,25 @@ function toggleMode() {
   const html = document.documentElement;
   html.classList.toggle('light');
 
+  // pegar a tag img
   const img = document.querySelector("#profile img");
 
+  // substituir a imagem
   if (html.classList.contains('light')) {
     img.setAttribute('src', './Assets/Avatar-light.png');
   } else {
     img.setAttribute('src', './Assets/Avatar.png');
   }
-}
 
+  /*
+  if (html.classList.contains('light')) {
+      html.classList.remove('light')
+  } else {
+      html.classList.add('light')
+  */
 
-/*
-if (html.classList.contains('light')) {
-    html.classList.remove('light')
-} else {
-    html.classList.add('light')
-*/
-
-
-
-
-//pegar a tag img
-
-// substituir a imagem
-
-//se tiver light mode, adicionar a imagem light
-//se tiver sem light mode, manter a imagem normal
+  // pegar a tag img
+  // substituir a imagem
+  // se tiver light mode, adicionar a imagem light
+  // se tiver sem light mode, manter a imagem normal
 }
