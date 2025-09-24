8afa7bd47dd23cc5f95bdf561cc7a09f49303ed3
Hilarionn <155191160+Hilarionn@users.noreply.github.com>
2025-09-24 19:26:46 -0300

    Update script.js - Avatar

--- script.js
+++ script.js
@@ -7,12 +7,11 @@ const img = document.querySelector("#profile img" )
 
 //substituir a imagem
 if (html.classList.contains('light')) {
-//se tiver light mode, adicionar a imagem light
-img.setAttribute('src', 'Assets/Avatar-Light.png')
+  img.setAttribute('src', './Assets/Avatar-Light.png')
 } else {
-    //se tiver sem light mode, manter a imagem normal
-    img.setAttribute('src', 'Assets/Avatar.png')
+  img.setAttribute('src', './Assets/Avatar.png')
 }
+
 
 
 /*
