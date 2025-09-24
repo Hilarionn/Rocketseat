dfe40a42ef14cdf48b3e8a26f0395279ffd70038
Gabriel Hilarion <email>
2025-09-24 19:11:47 -0300

    Avatar Case

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ const img = document.querySelector("#profile img" )
 //substituir a imagem
 if (html.classList.contains('light')) {
 //se tiver light mode, adicionar a imagem light
-img.setAttribute('src', 'Assets/avatar-Light.png')
+img.setAttribute('src', 'Assets/Avatar-Light.png')
 } else {
     //se tiver sem light mode, manter a imagem normal
-    img.setAttribute('src', 'Assets/avatar.png')
+    img.setAttribute('src', 'Assets/Avatar.png')
 }
 
 
