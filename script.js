8843f6cef61e4b5fc53f847529583be616ca939a
Hilarionn <155191160+Hilarionn@users.noreply.github.com>
2025-09-24 19:40:52 -0300

    Update script.js -rever

--- script.js
+++ script.js
@@ -11,16 +11,4 @@ function toggleMode() {
   } else {
     img.setAttribute('src', './Assets/Avatar.png');
   }
-
-  /*
-  if (html.classList.contains('light')) {
-      html.classList.remove('light')
-  } else {
-      html.classList.add('light')
-  */
-
-  // pegar a tag img
-  // substituir a imagem
-  // se tiver light mode, adicionar a imagem light
-  // se tiver sem light mode, manter a imagem normal
 }
